7907596e173a09f35f31249872958b35d5acee45
fatemehMrd <fatemeh.moradi74@gmail.com>
2022-11-09 11:09:46 +0200

    feat: Setup server and eslint rules

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world!');
+import Fastify from 'fastify';
+const app = Fastify({ logger: true });
+
+app.get('/', function (request, reply) {
+    reply.send({ hello: 'word' });
+});
+
+async function start() {
+    try {
+        await app.listen({ port: 3000, host: '0.0.0.0' });
+    } catch (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+}
+
+start();
